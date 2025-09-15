82c4378f568bac45f61db07d84ca7cf517469fc1
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-09-15 09:44:45 +0400

    tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,8 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
 
 router = DefaultRouter()
-router.register(r"task", TaskViewSet)
+
+router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path('', include(router.urls)),
 ]
