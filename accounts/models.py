a8737b1828bbd5e8ac3744b88e00ea2847ffc332
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-09-15 14:18:07 +0400

    user

--- accounts/models.py
+++ accounts/models.py
@@ -43,7 +43,7 @@ class User(AbstractUser):
     )
 
     def __str__(self):
-        return f" ({self.get_role_display()})"
+        return f"{self.first_name} {self.last_name} ({self.get_role_display()})"
 
     @property
     def assigner_role(self):
