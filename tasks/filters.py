e3013be9249f726b2f5ad49036aff3964b9ff7f5
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-09-16 10:37:37 +0400

    filters

---
+++ tasks/filters.py
@@ -0,0 +1,30 @@
+# tasks/filters.py
+
+from django_filters import rest_framework as filters
+from django.db.models import Q
+from .models import Task
+
+class TaskFilter(filters.FilterSet):
+    start_date_after = filters.DateFilter(field_name='start_date', lookup_expr='gte')
+    due_date_before = filters.DateFilter(field_name='due_date', lookup_expr='lte')
+
+    department = filters.NumberFilter(field_name='assignee__department__id')
+
+    search = filters.CharFilter(method='filter_by_search', label="Search in title and description")
+
+    class Meta:
+        model = Task
+        fields = [
+            'status',
+            'priority',
+            'assignee',
+            'department',
+            'start_date_after',
+            'due_date_before',
+            'search',
+        ]
+    
+    def filter_by_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(title__icontains=value) | Q(description__icontains=value)
+        )
