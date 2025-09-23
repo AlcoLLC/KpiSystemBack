dc56a9447712a7981bc9b8e62546b0ee5c8f0cc0
Aygun Amir <109809956+AmiraslanovaAygun@users.noreply.github.com>
2025-09-23 16:25:55 +0400

    task-homepage

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, TaskVerificationView, AssignableUserListView
+from .views import TaskViewSet, TaskVerificationView, AssignableUserListView, MonthlyTaskStatsView, PriorityTaskStatsView
 
 router = DefaultRouter()
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('tasks/verify/<str:token>/', TaskVerificationView.as_view(), name='task-verify'),
     path('assignable-users/', AssignableUserListView.as_view(), name='assignable-user-list'),
+    path('stats/monthly/', MonthlyTaskStatsView.as_view(), name='monthly-task-stats'),
+    path('stats/priority/', PriorityTaskStatsView.as_view(), name='priority-task-stats'),
 ]
