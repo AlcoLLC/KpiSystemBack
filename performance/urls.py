e946c6dbafcf1306b99247797989a779d42a497e
Aygun Amir <109809956+AmiraslanovaAygun@users.noreply.github.com>
2025-10-03 11:51:53 +0400

    performance

--- performance/urls.py
+++ performance/urls.py
@@ -3,5 +3,6 @@ from .views import SubordinateListView, PerformanceSummaryView
 
 urlpatterns = [
     path('subordinates/', SubordinateListView.as_view(), name='subordinate-list'),
+    path('summary/me/', PerformanceSummaryView.as_view(), name='my-performance-summary'),
     path('summary/<slug:slug>/', PerformanceSummaryView.as_view(), name='performance-summary'),
 ]
