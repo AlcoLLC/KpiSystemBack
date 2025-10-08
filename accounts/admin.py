80f6d928fb119ea781c4217475d8dc6e7f774195
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-10-08 12:46:38 +0400

    user

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,11 +5,10 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     fieldsets = BaseUserAdmin.fieldsets + (
-        ('Əlavə Məlumatlar', {'fields': ('role', 'department', 'profile_photo', 'phone_number', 'led_departments')}),
+        ('Əlavə Məlumatlar', {'fields': ('role', 'department', 'profile_photo', 'phone_number')}),
     )
     
     # Siyahıda görünəcək sahələr
-    filter_horizontal = ('led_departments',)
     list_display = ("id", "username", "email", "role", "first_name", "last_name", "is_staff")
     list_filter = ("role", "is_staff", "is_superuser", "groups")
     search_fields = ('username', 'email', 'first_name', 'last_name')
@@ -21,9 +20,8 @@ class DepartmentAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'manager', 'display_leads')
     list_filter = ('manager',) 
     search_fields = ('name',)
-    filter_horizontal = ('leads',)
+    filter_horizontal = ('lead',)
 
     def display_leads(self, obj):
-        return ", ".join([user.get_full_name() for user in obj.leads.all()])
-
+        return ", ".join([user.get_full_name() for user in obj.lead.all()])
     display_leads.short_description = 'Rəhbərlər (Leads)'
