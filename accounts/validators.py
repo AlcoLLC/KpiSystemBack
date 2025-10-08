3f27077e4e2093448f5e664035ed966c1e267d03
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-10-08 16:54:52 +0400

    user

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,7 +1,13 @@
 from django.core.exceptions import ValidationError
+import os
 
-def validate_svg(file):
-    if not file.name.endswith('.svg'):
-        raise ValidationError("Desteklenmeyen dosya uzantısı. Yalnızca .svg kabul edilir.")
-    if file.content_type != 'image/svg+xml':
-         raise ValidationError("Geçersiz dosya türü.")
+def validate_file_type(file):
+    allowed_extensions = ['.jpg', '.jpeg', '.png', '.webp', '.svg']
+    allowed_mime_types = ['image/jpeg', 'image/png', 'image/webp', 'image/svg+xml']
+    
+    ext = os.path.splitext(file.name)[1].lower()
+    if ext not in allowed_extensions:
+        raise ValidationError(f"Desteklenmeyen dosya uzantısı: '{ext}'. İcazə verilənlər: {', '.join(allowed_extensions)}")
+
+    if file.content_type not in allowed_mime_types:
+        raise ValidationError(f"Geçersiz dosya türü: '{file.content_type}'.")
