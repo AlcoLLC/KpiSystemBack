3e5224fe3c68688617487def2e92510c5195d70d
Aygun Amir <109809956+AmiraslanovaAygun@users.noreply.github.com>
2025-10-09 17:33:17 +0400

    performance-charts-kpi

--- performance/urls.py
+++ performance/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import SubordinateListView, PerformanceSummaryView, KpiMonthlySummaryView
+from .views import SubordinateListView, PerformanceSummaryView, KpiMonthlySummaryView, UserKpiScoreView
 
 urlpatterns = [
     path('subordinates/', SubordinateListView.as_view(), name='subordinate-list'),
     path('summary/me/', PerformanceSummaryView.as_view(), name='my-performance-summary'),
     path('summary/<slug:slug>/', PerformanceSummaryView.as_view(), name='performance-summary'),
     path('kpi-summary/<slug:slug>/', KpiMonthlySummaryView.as_view(), name='kpi-monthly-summary'),
+    path('kpi-score/<slug:slug>/', UserKpiScoreView.as_view(), name='user-kpi-score'),
+
 
 ]
